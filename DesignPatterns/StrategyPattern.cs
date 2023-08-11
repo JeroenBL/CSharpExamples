98ec20122fb392a11b1fdabdb12df68745ca19d4
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2023-08-11 09:24:00 +0200

    Update StrategyPattern.cs

--- DesignPatterns/StrategyPattern.cs
+++ DesignPatterns/StrategyPattern.cs
@@ -1,9 +1,45 @@
+// StrategyPattern.
 // Defines a family of algorithms encapsulating each one and making each interchangable.
+// Changes behavior of a class without modifying or extending it.
+// In this case, we have a base class 'BetterDuckClass' with two methods. Fly() and Quack(). 
+// Because these are both interfaces and not concrete implementations, we can create different
+// types of ducks(s) and inject specific fly and quack behaviors. 
 
-namespace InterfacesDemo
+namespace StategyPattern
 {
     public class Program
     {
+       public static void Main(string[] args)
+        {
+            // Create fly behaviours
+            IFlyable flyingNormal = new FlyingNormal();
+            IFlyable flyingHigh = new FlyingHigh();
+
+            // Create quack behaviours
+            IQuackable quackingLoud = new QuackingLoud();
+            IQuackable squeek = new Squeak();
+
+            // Create wildDuck (flying normal, quacking loud)
+            Console.WriteLine("WildDuck (flying normal, quacking loud");
+            var betterWildDuck = new BetterDuckClass(flyingNormal, quackingLoud);
+            betterWildDuck.Fly();
+            betterWildDuck.Quack();
+            Console.WriteLine("");
+
+            // Create mountainDuck (flying high, quacking loud)
+            Console.WriteLine("MountainDuck (flying high, quacking loud");
+            var betterMountainDuck = new BetterDuckClass(flyingHigh, quackingLoud);
+            betterMountainDuck.Fly();
+            betterMountainDuck.Quack();
+            Console.WriteLine("");
+
+            // Create rubberDuck (won't fly, squeeks)
+            Console.WriteLine("RubberDuck (won't fly, squeeks)");
+            var betterRubberDuck = new BetterDuckClass(null, squeek);
+            betterRubberDuck.Quack();
+            Console.WriteLine("");
+        }
+
         // Create interface for fly().
         public interface IFlyable
         {
@@ -73,40 +109,6 @@ namespace InterfacesDemo
             {
                 _quack.Quack();
             }
-        }
-
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Better approach using interfaces");
-
-            // Create fly behaviours
-            IFlyable flyingNormal = new FlyingNormal();
-            IFlyable flyingHigh = new FlyingHigh();
-            IFlyable wontFly = new WontFly();
-
-            // Create quack behaviours
-            IQuackable quackingLoud = new QuackingLoud();
-            IQuackable squeek = new Squeak();
-
-            // Create wildDuck (flying normal, quacking loud)
-            Console.WriteLine("WildDuck-Interface (flying normal, quacking loud");
-            var betterWildDuck = new BetterDuckClass(flyingNormal, quackingLoud);
-            betterWildDuck.Fly();
-            betterWildDuck.Quack();
-            Console.WriteLine("");
-
-            // Create mountainDuck (flying high, quacking loud)
-            Console.WriteLine("MountainDuck-Interface (flying high, quacking loud");
-            var betterMountainDuck = new BetterDuckClass(flyingHigh, quackingLoud);
-            betterMountainDuck.Fly();
-            betterMountainDuck.Quack();
-            Console.WriteLine("");
-
-            // Create rubberDuck (won't fly, squeeks)
-            Console.WriteLine("RubberDuck-Interface (won't fly, squeeks");
-            var betterRubberDuck = new BetterDuckClass(null, squeek);
-            betterRubberDuck.Quack();
-            Console.WriteLine("");
         }
     }
 }
